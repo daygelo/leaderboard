d30225395bdd4264e89863ec9b662a16e046acde
Angelo Djeumene <80226633+daygelo@users.noreply.github.com>
2024-12-10 23:32:20 -0600

    Initial commit.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -13,9 +13,24 @@
   let increment: number = $derived(inputValue.match(/^-?\d+$/) || inputValue.match(/^-?\d*\.\d+$/) ? +inputValue : 0);
 </script>
 
-<main class='w-full min-h-screen bg-yellow-500 text-4xl'>
+<svelte:head>
+  <title>Leaderboard</title>
+</svelte:head>
+
+<main class='w-full min-h-screen bg-black text-white font-array select-none'>
+  <input
+    class='fixed left-8 mt-8 text-4xl text-green-500 outline-none placeholder:text-green-500/0 hover:placeholder:text-green-500/25 placeholder:transition'
+    placeholder='type here'
+    bind:value={inputValue}
+    onkeyup={event => {
+        if (event.key === 'Enter') {
+          items[nanoid()] = { name: inputValue, score: 0 };
+          inputValue = '';
+        }
+      }}
+  />
   <section class='w-full max-w-4xl mx-auto py-32'>
-    <div>
+    <div class='space-y-4'>
       {#each Object.entries(items).sort((a, b) => b[1].score - a[1].score) as [id, item], i (id)}
         <div animate:flip>
           <Item
@@ -33,16 +48,5 @@
       {/each}
     </div>
 
-    <input
-      class='w-full mt-8 text-white outline-none placeholder:text-white/0 hover:placeholder:text-white/25 placeholder:transition'
-      placeholder='type here'
-      bind:value={inputValue}
-      onkeyup={event => {
-        if (event.key === 'Enter') {
-          items[nanoid()] = { name: inputValue, score: 0 };
-          inputValue = '';
-        }
-      }}
-    />
   </section>
 </main>
--- src/routes/Item.svelte
+++ src/routes/Item.svelte
@@ -12,12 +12,14 @@
       update: () => void,
       remove: () => void,
     } = $props();
+
+    const placeStyle = $derived(place === 1 ? 'text-amber-500' : place === 2 ? 'text-slate-400' : place === 3 ? 'text-red-500' : 'text-gray-700');
 </script>
 
-<div class='group relative flex items-center gap-8 px-8 py-8 bg-white rounded-4xl'>
-    <p class='flex justify-center items-center size-16 rounded-full bg-black text-white font-bold'>{place}</p>
-    <p class='mr-auto font-bold'>{name}</p>
-    <p class='text-6xl font-bold'>
+<div class='group relative flex items-center gap-12 px-8 py-8 text-6xl'>
+    <p class={`flex justify-center items-center size-16 rounded-full ${placeStyle} text-8xl font-array font-bold`}>{place}</p>
+    <p class='mr-auto'>{name}</p>
+    <p class='font-array font-bold'>
         {
           new Intl.NumberFormat('en-CA', {
             style: 'currency',
@@ -25,7 +27,7 @@
           }).format(score)
         }
     </p>
-    <div class='absolute left-[calc(100%+16px)] flex space-x-2 opacity-0 group-hover:opacity-100 transition duration-50'>
+    <div class='absolute left-[calc(100%+16px)] flex space-x-2 opacity-0 group-hover:opacity-100 transition duration-50 text-4xl'>
         <button class='flex justify-center items-center size-10 rounded-full text-white/50' onclick={update}>
             +
         </button>
